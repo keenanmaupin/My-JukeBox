c3aca438251281d51fafc99f2fdcbbeaccae0fbc
keenan maupin <maupin.keenan97@gmail.com>
2024-12-08 23:58:05 -0800

    home page issue 5.0

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename= {import.meta.env.BASE_URL}>
+      <Routes>
+        <Routte path= "*" element= {<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
